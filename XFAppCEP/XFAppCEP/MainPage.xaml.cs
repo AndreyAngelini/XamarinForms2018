3172b3b328c82483d76c04d3c0bc4395e7234a2e
AndreyAngelini <andreyangelini@gmail.com>
2021-07-27 15:58:46 -0300

    First commit

---
+++ XFAppCEP/XFAppCEP/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFAppCEP.Service;
+using XFAppCEP.Service.Model;
+
+namespace XFAppCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            imgLogo.Source = "Logo.png";
+            btnBuscarCep.Clicked += BtnBuscarCep_Clicked; 
+        }
+
+        private void BtnBuscarCep_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string cep = txtCep.Text.Trim();
+
+                Address endereco = new Address();
+                viaCEPServico buscarCEP = new viaCEPServico();
+
+                string mensagemErro = "";
+                if (IsValidCEP(cep, out mensagemErro))
+                {
+                    endereco = buscarCEP.BuscarEnderecoViaCEP(cep);
+                    if (endereco != null)
+                    {
+                        lblMensagem.Text = string.Format("Endereço: {0}\nBairro: {1}\nCidade: {2}\nUF: {3}\nIBGE: {4}",
+                                              endereco.logradouro,
+                                              endereco.bairro,
+                                              endereco.localidade,
+                                              endereco.uf,
+                                              endereco.ibge);
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro", "Endereço não encontrado para o cep:" + cep , "Ok");
+                    }
+                }
+                else
+                {
+                    DisplayAlert("Erro", mensagemErro, "Ok");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", "Problemas para buscar o CEP. \nErro: " + ex.Message,"OK");
+            }
+
+        }
+
+        private bool IsValidCEP(string cep, out string mesnsagem )
+        {
+            bool retorno = true;
+            mesnsagem = "";
+            if (cep.Length != 8)
+            {
+                mesnsagem += "\nO CEP deve conter 8 dígitos.";
+                retorno = false;
+            }
+
+            double dcep = new double();
+            if (!double.TryParse(cep, out dcep))
+            {
+                mesnsagem += "\nO CEP deve conter apenas números.";
+                retorno = false;
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ XFAppCEP/XFAppCEP/Service/viaCEPServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using XFAppCEP.Service.Model;
+using Newtonsoft.Json;
+namespace XFAppCEP.Service
+{
+    public class viaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public Address BuscarEnderecoViaCEP(string cep)
+        {
+            Address endereco = new Address();
+            string NovoEnderecoUrl = string.Format(EnderecoURL, cep);
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoUrl);
+            endereco = JsonConvert.DeserializeObject<Address>(conteudo);
+            if (endereco.cep == null)
+                return null;
+
+            return endereco;
+        }
+    }
+}
